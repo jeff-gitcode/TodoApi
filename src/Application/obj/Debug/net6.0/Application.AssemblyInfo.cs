b2e053dfcf1c562f913cdb6a03ef265349879663
jeff-gitcode <jeff.gitcode@gmail.com>
2025-03-12 12:09:15 +1100

    updated gitignore

--- src/Application/obj/Debug/net6.0/Application.AssemblyInfo.cs
+++ src/Application/obj/Debug/net6.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcdb983fd29d261984bc2b1d73abe11bbd43fe28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49243423276d549543537f74df9ca13d8357a216")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
